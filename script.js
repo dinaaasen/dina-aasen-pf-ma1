8e08750701862944135ecde7c3c9dd43c7d8fec1
dinaaasen <65859588+dinaaasen@users.noreply.github.com>
2020-05-24 18:56:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+// question 1
+
+// var firstVar;
+
+// question 2
+
+// var name = "Dina";
+
+// question 3
+
+// var number = 8;
+
+// question 4
+
+//
+/* var firstNumber = 20;
+   var secondNumber = 5;
+   console.log(firstNumber / secondNumber);
+*/
+
+// question 5
+
+// var animal = "frog";
+   typeof(animal);
+// "frog"   
+
+// question 6
+
+//var orderHasShipped = true;
+
+// question 7
+
+//
+if (orderHasShipped) === true {
+  console.log("Order Has Shipped");
+  }
+else {
+  console.log("Order Has Not Shipped);
+  }
+
+// question 8
+
+// for (var count = 0; count <= 9; count++) {
+   console.log(count);
+   }
